2ba3a47be7b701638005b0349d5bdf43d680a468
Rockodrilo <edgar.ricoromero@outlook.com>
2025-05-29 22:23:26 -0600

    se hace paquete de api servicio y sus correspondientes archivos

--- app/src/main/java/com/ebc/cliente_servicio_anfibios/controller/AnfibiosApiService.kt
+++ app/src/main/java/com/ebc/cliente_servicio_anfibios/controller/AnfibiosApiService.kt
@@ -1,5 +1,6 @@
 package com.ebc.cliente_servicio_anfibios.controller
 
+import com.ebc.cliente_servicio_anfibios.models.Anfibio
 import retrofit2.http.GET
 
 //declaro conportamientos de interface que solo es GET
